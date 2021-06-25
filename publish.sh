fc89974fcd059c23555133aefcdc3ccfe8c00728
Dan Sheldon <sheldon@cs.umass.edu>
2021-06-25 09:49:43 -0400

    Set destination files to be group writeable

--- publish.sh
+++ publish.sh
@@ -7,4 +7,4 @@ DST=${3:-/var/www/html/roost/}
 FULLPATH=$DST/$NAME
 
 ssh $HOST mkdir -p $FULLPATH
-rsync -avz --include "index.html" --include "dist**" --include "data**" --exclude "*" . $HOST:$FULLPATH/
+rsync -avz --include "index.html" --include "dist**" --include "data**" --exclude "*" --chmod=ug=rwX . $HOST:$FULLPATH/
