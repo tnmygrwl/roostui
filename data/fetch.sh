a0ab3a709a6a1f491f3be686d5190d98daa8d361
wenlongzhao094 <wenlong.zhao.97@gmail.com>
2022-05-05 10:05:02 -0400

    some more deployment

--- data/fetch.sh
+++ data/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NAME=${1:-test}
-SRC=${2:-swarm2:/mnt/nfs/scratch1/wenlongzhao/roosts_data}
+SRC=${2:-swarm:/mnt/nfs/scratch1/wenlongzhao/roosts_data}
 
 echo "name is $NAME"
 rsync -avz $SRC/$NAME/ui/scans_and_tracks/ $NAME/
